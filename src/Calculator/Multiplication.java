4a4ba0bfec1232a78cb246451a25bd322311d13f
vineeljillapally <vineeljillapally@gmail.com>
2026-09-19 12:27:50 +0530

    THIS IS MULTIPLICATION COMMIT

---
+++ src/Calculator/Multiplication.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+public class Multiplication {
+	 static public void multiply(int a,int b) {
+		System.out.println(a*b);
+	}
+	public static void main(String[] args) {
+		multiply(10,5);
+	}
+
+}
